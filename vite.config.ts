f36e64b6693559f4c6993879d7b7c13c3ed1534a
Villegas, Edgar <Edgar.Villegas@mckesson.com>
2024-08-19 20:04:28 -0600

    Add vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
+import { configDefaults } from 'vitest/config'
 import react from '@vitejs/plugin-react-swc'
+import { test } from 'vitest';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  test: {
+    environment: 'happy-dom',
+    exclude: [
+      ...configDefaults.exclude,
+      "tests/*"
+    ],
+  },
+  plugins: [react()],
 })
